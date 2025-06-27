a515548acda04c8ecc0cf4ecd006d03f7ec913c6
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-06-26 21:58:29 -0700

    adding on boarding images

--- lib/core/resources/assets_manager.dart
+++ lib/core/resources/assets_manager.dart
@@ -1,7 +1,17 @@
 const String imagePath = 'assets/images';
 
 abstract class AssetsManager {
+  // splash images
   static const splashBackground = '$imagePath/splash_background.png';
+
+  // on boarding images
+  static const doaa = '$imagePath/doaa.png';
+  static const quran = '$imagePath/quran.png';
+  static const radio = '$imagePath/radio.png';
+
+  // home images
   static const homeBackground = '$imagePath/home_background.png';
+
+  // radio images
   static const radioBackground = '$imagePath/radio_background.png';
 }
