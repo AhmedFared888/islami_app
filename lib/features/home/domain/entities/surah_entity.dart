5a183014f244cb6b8b038faa5605dff45ec89459
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-06-28 22:33:25 -0700

    adding surah entity

---
+++ lib/features/home/domain/entities/surah_entity.dart
@@ -0,0 +1,12 @@
+class SurahEntity {
+  final int number;
+  final String name;
+  final int versesNumber;
+  final String arabicName;
+  SurahEntity({
+    required this.number,
+    required this.name,
+    required this.versesNumber,
+    required this.arabicName,
+  });
+}
