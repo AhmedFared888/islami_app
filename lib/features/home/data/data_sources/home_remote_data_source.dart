02bea4bcb42663e112001824d4b62015dd9286ad
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-06-29 19:05:46 -0700

    implement remote fetch Surahs

--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -1,3 +1,5 @@
+import 'package:islami/core/utils/api_service.dart';
+import 'package:islami/features/home/data/models/surah_model.dart';
 import 'package:islami/features/home/domain/entities/surah_entity.dart';
 
 abstract class HomeRemoteDataSource {
@@ -5,9 +7,16 @@ abstract class HomeRemoteDataSource {
 }
 
 class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
+  final ApiService apiService;
+
+  HomeRemoteDataSourceImpl(this.apiService);
   @override
-  Future<List<SurahEntity>> fetchSurahs() {
-    // TODO: implement fetchSurahs
-    throw UnimplementedError();
+  Future<List<SurahEntity>> fetchSurahs() async {
+    var data = await apiService.get(endPoint: 'surah');
+    List<SurahEntity> surahs = [];
+    for (var surahMap in data['data']) {
+      surahs.add(SurahModel.fromJson(surahMap));
+    }
+    return surahs;
   }
 }
