2bbe08020af85de5a3a584a4e278756d92405294
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-06-29 19:50:30 -0700

    Register surah entity adapter and open surahs box

---
+++ lib/constants.dart
@@ -0,0 +1,1 @@
+const kSurahsBox = 'surahs_box';
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:islami/constants.dart';
 import 'package:islami/core/resources/routes_manager.dart';
 import 'package:islami/core/resources/theme_manager.dart';
+import 'package:islami/features/home/domain/entities/surah_entity.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  await Hive.initFlutter();
+  Hive.registerAdapter(SurahEntityAdapter());
+  await Hive.openBox(kSurahsBox);
+
   runApp(Islami());
 }
 
