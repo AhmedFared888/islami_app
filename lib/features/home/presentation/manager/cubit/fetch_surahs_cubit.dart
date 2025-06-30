5991ee2cadbb00bd088ac0de74b187020fed5dc9
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-06-30 04:19:12 -0700

    Implement cubit fetch surahs method

--- lib/features/home/presentation/manager/cubit/fetch_surahs_cubit.dart
+++ lib/features/home/presentation/manager/cubit/fetch_surahs_cubit.dart
@@ -1,9 +1,25 @@
 import 'package:bloc/bloc.dart';
 import 'package:islami/features/home/domain/entities/surah_entity.dart';
+import 'package:islami/features/home/domain/usecases/home_use_case.dart';
 import 'package:meta/meta.dart';
 
 part 'fetch_surahs_state.dart';
 
 class FetchSurahsCubit extends Cubit<FetchSurahsState> {
-  FetchSurahsCubit() : super(FetchSurahsInitial());
+  FetchSurahsCubit(this.homeUseCase) : super(FetchSurahsInitial());
+
+  final HomeUseCase homeUseCase;
+
+  Future<void> fetshSurahs() async {
+    emit(FetchSurahsLoading());
+    var result = await homeUseCase.execute();
+    result.fold(
+      (failure) {
+        emit(FetchSurahsFailure(failure.message));
+      },
+      (surahs) {
+        emit(FetchSurahsSuccesse(surahs));
+      },
+    );
+  }
 }
