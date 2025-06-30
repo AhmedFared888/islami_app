5fbf1c553573acdf3a9b3a5b55f46a0c99f32b71
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-06-30 05:47:10 -0700

    add the type of hive box to solve : "HiveError: The box "surahs_box" is already open and of type Box<SurahEntity>."

--- lib/core/utils/functions/save_surahs.dart
+++ lib/core/utils/functions/save_surahs.dart
@@ -2,6 +2,6 @@ import 'package:hive/hive.dart';
 import 'package:islami/features/home/domain/entities/surah_entity.dart';
 
 void saveSurahsData(List<SurahEntity> surahs, String boxName) {
-  var box = Hive.box(boxName);
+  var box = Hive.box<SurahEntity>(boxName);
   box.addAll(surahs);
 }
