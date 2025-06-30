305ee7bf0ce06356962605e3684a9684ed9de900
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-06-30 04:43:58 -0700

    adding bloc provider to home view

--- lib/core/resources/routes_manager.dart
+++ lib/core/resources/routes_manager.dart
@@ -1,6 +1,14 @@
+import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:islami/core/utils/api_service.dart';
+import 'package:islami/features/home/data/data_sources/home_local_data_source.dart';
+import 'package:islami/features/home/data/data_sources/home_remote_data_source.dart';
+import 'package:islami/features/home/data/repos/home_repo_impl.dart';
+import 'package:islami/features/home/domain/usecases/home_use_case.dart';
+import 'package:islami/features/home/presentation/manager/cubit/fetch_surahs_cubit.dart';
+import 'package:islami/features/home/presentation/views/home_view.dart';
 import 'package:islami/features/home/presentation/views/surah_details_view.dart';
 import 'package:islami/features/login/data/repos/login_repository_impl.dart';
 import 'package:islami/features/login/domain/usecases/login_usecase.dart';
@@ -16,6 +24,7 @@ class RoutesManager {
   static const String splashRoute = '/';
   static const String onBoardingRoute = '/onBoarding';
   static const String loginRoute = '/login';
+  static const String mainRoute = '/main';
   static const String homeRoute = '/home';
   static const String surahDetailsRoute = '/surahDetails';
   static const String radioRoute = '/radio';
@@ -39,7 +48,23 @@ class RoutesManager {
           child: const LoginView(),
         ),
       ),
-      GoRoute(path: homeRoute, builder: (context, state) => const MainView()),
+      GoRoute(path: mainRoute, builder: (context, state) => const MainView()),
+      GoRoute(
+        path: homeRoute,
+        builder: (context, state) => BlocProvider(
+          create: (context) => FetchSurahsCubit(
+            HomeUseCase(
+              HomeRepoImpl(
+                homeLocalDataSource: HomeLocalDataSourceImpl(),
+                homeRemoteDataSource: HomeRemoteDataSourceImpl(
+                  ApiService(Dio()),
+                ),
+              ),
+            ),
+          ),
+          child: const HomeView(),
+        ),
+      ),
       GoRoute(
         path: surahDetailsRoute,
         builder: (context, state) {
