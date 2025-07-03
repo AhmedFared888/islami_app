0e665f34afaedbd59920134898763b324383f6b0
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-02 22:40:53 -0700

    create Surah Details UseCase

--- lib/features/home/domain/usecases/surah_details_use_case.dart
+++ lib/features/home/domain/usecases/surah_details_use_case.dart
@@ -1,15 +1,16 @@
 import 'package:dartz/dartz.dart';
 import 'package:islami/core/errors/failure.dart';
-import 'package:islami/core/use_cases/no_param_use_case.dart';
-import 'package:islami/features/home/domain/repos/surah_details_repo.dart';
+import 'package:islami/core/use_cases/use_case.dart';
+import 'package:islami/features/home/domain/entities/surah_details_entity.dart';
+import 'package:islami/features/home/domain/repos/home_repo.dart';
 
-class SurahDetailsUseCase extends UseCase {
-  final SurahDetailsRepo surahDetailsRepo;
+class SurahDetailsUseCase extends UseCase<List<SurahDetailsEntity>, int> {
+  final HomeRepo homeRepo;
 
-  SurahDetailsUseCase(this.surahDetailsRepo);
+  SurahDetailsUseCase(this.homeRepo);
+
   @override
-  Future<Either<Failure, dynamic>> execute() {
-    // if I want to check permssion or any code I want to do here
-    return surahDetailsRepo.fetchSurahDetails();
+  Future<Either<Failure, List<SurahDetailsEntity>>> execute(int surahId) {
+    return homeRepo.fetchSurahDetails(surahId);
   }
 }
