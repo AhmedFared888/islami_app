8ba7386da528698b55f1fd9faa8c1a94c7a5d481
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-06 18:42:25 -0700

    create azkar list item model

---
+++ lib/features/azkar/data/models/azkar_list_item_model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class AzkarListItemModel {
+  final String title;
+  final Widget customDivider;
+
+  AzkarListItemModel({required this.title, required this.customDivider});
+}
