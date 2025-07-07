c0bc67fc91d2ac2adb77d4d1b5836092039f1eb6
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-07 13:18:05 -0700

    create azkar details view and view body

--- lib/features/azkar/presentation/views/azkar_view.dart
+++ lib/features/azkar/presentation/views/azkar_view.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:islami/features/azkar/presentation/widgets/azkar_view_body.dart';
+import 'package:islami/features/azkar/presentation/widgets/azkar_details_view_body.dart';
 
 class AzkarView extends StatelessWidget {
   const AzkarView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const AzkarViewBody();
+    return const AzkarDetailsViewBody();
   }
 }
---
+++ lib/features/azkar/presentation/widgets/azkar_details_view_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AzkarDetailsViewBody extends StatelessWidget {
+  const AzkarDetailsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('you are in azkar details now')),
+    );
+  }
+}
