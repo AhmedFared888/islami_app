4782de473b587c0ace6fa892996e86a64c40750e
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-07 14:24:19 -0700

    create azkar details entity

---
+++ lib/features/azkar/domain/entities/azkar_details_entity.dart
@@ -0,0 +1,8 @@
+class AzkarDetailsEntity {
+  final String category;
+  final String content;
+  final int count;
+  final String description;
+
+  AzkarDetailsEntity(this.category, this.content, this.count, this.description);
+}
