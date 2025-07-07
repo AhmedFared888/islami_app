9098d13cd187b18b1e10383f73608d31bb107b33
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-07 14:35:51 -0700

    create azkar details usecase

---
+++ lib/features/azkar/domain/usecases/azkar_details_use_case.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:islami/core/errors/failure.dart';
+import 'package:islami/core/use_cases/use_case.dart';
+import 'package:islami/features/azkar/domain/entities/azkar_details_entity.dart';
+import 'package:islami/features/azkar/domain/repos/azkar_details_repo.dart';
+
+class AzkarDetailsUseCase extends UseCase<AzkarDetailsEntity, String> {
+  final AzkarDetailsRepo azkarDetailsRepo;
+
+  AzkarDetailsUseCase(this.azkarDetailsRepo);
+
+  @override
+  Future<Either<Failure, AzkarDetailsEntity>> execute(String category) {
+    return azkarDetailsRepo.fetchAzkar(category: category);
+  }
+}
