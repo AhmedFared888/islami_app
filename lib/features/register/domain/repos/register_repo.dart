ec528201f0290e2ed145cf642e4aec1aa7d26bff
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-13 03:50:21 -0700

    create register repo

---
+++ lib/features/register/domain/repos/register_repo.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:islami/core/errors/failure.dart';
+import 'package:islami/features/register/domain/entites/user_entity.dart';
+
+abstract class RegisterRepo {
+  Future<Either<Failure, UserEntity>> register({
+    required String email,
+    required String password,
+  });
+}
