4bdab6501a42730bcbb5e8472418176cbb337389
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-16 19:04:42 -0700

    setting section (listtile && listview)

---
+++ lib/features/settings/presentation/views/settings_section.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:islami/features/settings/presentation/widgets/settings_section_body.dart';
+
+class SettingsSection extends StatelessWidget {
+  const SettingsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SettingsSectionBody();
+  }
+}
---
+++ lib/features/settings/presentation/widgets/settings_section_body.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:islami/core/resources/color_manager.dart';
+import 'package:islami/core/resources/strings_manager.dart';
+import 'package:islami/core/resources/styles_manager.dart';
+import 'package:islami/core/widgets/custom_divider.dart';
+import 'package:islami/features/settings/data/list_tile_model.dart';
+
+class SettingsSectionBody extends StatelessWidget {
+  SettingsSectionBody({super.key});
+
+  List<ListTileModel> list = [
+    ListTileModel(
+      leading: const Icon(Icons.language),
+      title: const Text(StringsManager.changeLanguage),
+      trailing: const Icon(Icons.arrow_forward_ios_rounded),
+      onTap: () {},
+    ),
+
+    ListTileModel(
+      leading: const Icon(Icons.logout_outlined),
+      title: const Text(StringsManager.logOut),
+      trailing: const Icon(Icons.arrow_forward_ios_rounded),
+      onTap: () {},
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ColorManager.backGroundColor,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text(
+              StringsManager.settings,
+              style: StylesManager.textStyle24(ColorManager.primaryColor),
+            ),
+            const CustomDivider(),
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  final item = list[index];
+                  return ListTile(
+                    leading: item.leading,
+                    title: item.title,
+                    titleTextStyle: StylesManager.textStyle14(
+                      ColorManager.primaryColor,
+                    ),
+                    trailing: item.trailing,
+                    onTap: item.onTap,
+                  );
+                },
+                itemCount: list.length,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
